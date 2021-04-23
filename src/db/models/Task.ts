4d1ff16395f4a6f249057e08142e1f313b8c625b
NonsoAmadi10 <nonsoamadi@aol.com>
2021-04-23 10:23:26 +0100

    bug: fix git rename error

---
+++ src/db/models/Task.ts
@@ -0,0 +1,31 @@
+import { DataTypes } from 'sequelize';
+import { sequelize} from '.'
+import TaskInstance from '../../interfaces/task';
+
+const Task = sequelize.define<TaskInstance>(
+  'Task', {
+
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    task: {
+      allowNull: true,
+      type: DataTypes.TEXT,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+    },
+
+    listId: {
+      allowNull: true,
+      type: DataTypes.UUID,
+    },
+  }
+)
+
+export default Task; 
